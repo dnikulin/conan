0c25839d9bd064bf17d28639959d0e054f6a63da
Dmitri Nikulin <dnikulin@gmail.com>
2011-02-02 18:54:20 +1100

    ConanZero: Refer to Conan by name in license block

--- ConanZero/ConanTypes.hh
+++ ConanZero/ConanTypes.hh
@@ -1,17 +1,19 @@
 // Copyright (c) 2011  Dmitri Nikulin
 //
-// This library is free software; you can redistribute it and/or
+// This file is part of Conan.
+//
+// Conan is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation; either
 // version 2.1 of the License, or (at your option) any later version.
 //
-// This library is distributed in the hope that it will be useful,
+// Conan is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// along with Conan.  If not, see <http://www.gnu.org/licenses/>.
 
 #ifndef CONANTYPES_HH
 #define CONANTYPES_HH
